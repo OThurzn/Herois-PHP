e3ddde54f25329e6d66f1dac147181ba332bb2d0
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-05-28 13:50:49 -0300

    28.05

--- index.php
+++ index.php
@@ -9,10 +9,12 @@
 </head>
 
 <body>
-    <!-- Botão para abrir a modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Abrir Formulário
-    </button>
+    <div class="container d-flex justify-content-center align-items-start position-sticky rounded-3 p-3">
+        <!-- Botão para abrir a modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            PESQUISAR HERÓI
+        </button>
+    </div>
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -25,8 +27,8 @@
                 <div class="modal-body">
                     <form action="index.php" method="post">
                         <label class="form-label">NOME DO HERÓI</label>
-                        <input type="text" name="campo_nome" value="<?php echo isset($nome) ? $nome : ''; ?>" class="form-control mb-3"
-                            placeholder="Ex.: Homem-Aranha">
+                        <input type="text" name="campo_nome" value="<?php echo isset($nome) ? $nome : ''; ?>"
+                            class="form-control mb-3" placeholder="Ex.: Homem-Aranha">
 
                         <label class="form-label">UNIVERSO</label>
 
@@ -42,8 +44,9 @@
                         </div>
 
                         <label class="form-label">QUANTAS VEZES DESEJA</label>
-                        <input type="number" name="campo_quantidade" value="<?php echo isset($quantidade) ? $quantidade : ''; ?>"
-                            class="form-control mb-3" placeholder="1..2..3">
+                        <input type="number" name="campo_quantidade"
+                            value="<?php echo isset($quantidade) ? $quantidade : ''; ?>" class="form-control mb-3"
+                            placeholder="1..2..3">
                         <button type="submit" class="btn btn-primary">Enviar</button>
                     </form>
                 </div>
@@ -56,11 +59,24 @@
         if (empty($_POST["campo_nome"]) || empty($_POST["rdb_universo"]) || empty($_POST["campo_quantidade"])) {
             echo "<h3 class='text-center mt-3'>Digite um nome, escolha um universo e informe a quantidade desejada.</h3>";
         } else {
-            $nome = $_POST["campo_nome"];
+            $nome = strtolower($_POST["campo_nome"]);
             $universo = $_POST["rdb_universo"];
             $quantidade = $_POST["campo_quantidade"];
 
-            echo "<p class='text-center mt-3'>O herói se chama $nome, pertence ao universo $universo e a quantidade desejada é $quantidade.</p>";
+            if ($nome == "pantera negra") {
+                echo "<img src='imgs/pantera-negra.png'>";
+            } elseif ($nome == "demolidor") {
+                echo "<img src='imgs/demolidor.png'>";
+            } elseif ($nome == "coisa") {
+                echo "<img src='imgs/coisa.png'>";
+            } elseif ($nome == "homem aranha" || $nome == "spiderman") {
+                echo "<img src='imgs/homem-aranha.png'>";
+            } elseif ($nome == "homem de ferro" || $nome == "ironman") {
+                echo "<img src='imgs/homem-de-ferro.png'>";
+            } else {
+                // Tratamento para o caso em que nenhum herói corresponde
+                echo "Nenhum herói correspondente encontrado.";
+            }
         }
     }
     ?>
